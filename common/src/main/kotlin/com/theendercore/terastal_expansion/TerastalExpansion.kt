25df19438829793869f132885e16456319d8df2f
TheEnderCore <theendercore@gmail.com>
2025-01-21 15:24:33 +0200

    - POKEMON_SENT_POST test nr 400

--- common/src/main/kotlin/com/theendercore/terastal_expansion/TerastalExpansion.kt
+++ common/src/main/kotlin/com/theendercore/terastal_expansion/TerastalExpansion.kt
@@ -1,5 +1,7 @@
 package com.theendercore.terastal_expansion
 
+import com.cobblemon.mod.common.api.events.CobblemonEvents.POKEMON_SENT_POST
+import com.cobblemon.mod.common.net.messages.client.pokemon.update.TeraTypeUpdatePacket
 import com.theendercore.terastal_expansion.misc.TerastalImplementation
 
 object TerastalExpansion {
@@ -8,5 +10,10 @@ object TerastalExpansion {
         tera.registerBlocks()
         tera.registerItems()
     }
-    fun events(){}
+
+    fun events() {
+        POKEMON_SENT_POST.subscribe {
+            it.pokemon.notify(TeraTypeUpdatePacket({ println("updating pokemon: ${it.pokemon.teraType.id}");it.pokemon }, it.pokemon.teraType))
+        }
+    }
 }
