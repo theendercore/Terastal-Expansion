d0ab4bdbda39e379bc3e1fde9916d62cda9302ee
TheEnderCore <theendercore@gmail.com>
2025-01-28 16:31:59 +0200

    - removed PokemonSelectingItem from the TeraOrbItem

--- common/src/main/kotlin/com/theendercore/terastal_expansion/item/TeraOrbItem.kt
+++ common/src/main/kotlin/com/theendercore/terastal_expansion/item/TeraOrbItem.kt
@@ -1,34 +1,10 @@
 package com.theendercore.terastal_expansion.item
 
-import com.cobblemon.mod.common.CobblemonSounds
-import com.cobblemon.mod.common.api.item.PokemonSelectingItem
 import com.cobblemon.mod.common.item.CobblemonItem
-import com.cobblemon.mod.common.pokemon.Pokemon
-import com.theendercore.terastal_expansion.client.net.TeraStateUpdatePacket
-import com.theendercore.terastal_expansion.misc.getTeraState
-import com.theendercore.terastal_expansion.misc.setTeraState
-import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.InteractionHand
-import net.minecraft.world.InteractionResultHolder
-import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.Level
 
-class TeraOrbItem : CobblemonItem(Properties().stacksTo(1)), PokemonSelectingItem {
-    override val bagItem = null
-    override fun canUseOnPokemon(pokemon: Pokemon) = true
-
-    override fun applyToPokemon(player: ServerPlayer, stack: ItemStack, pokemon: Pokemon)
-            : InteractionResultHolder<ItemStack> {
-        pokemon.entity?.playSound(CobblemonSounds.FISHING_ROD_CAST, 1F, 1F)
-        val state = !pokemon.getTeraState()
-        pokemon.setTeraState(state)
-        pokemon.notify(TeraStateUpdatePacket({ pokemon }, state))
-        return InteractionResultHolder.success(stack)
-    }
-
-    override fun use(world: Level, user: Player, hand: InteractionHand): InteractionResultHolder<ItemStack> {
-        return if (user is ServerPlayer) use(user, user.getItemInHand(hand))
-        else InteractionResultHolder.success(user.getItemInHand(hand))
-    }
+class TeraOrbItem : CobblemonItem(Properties().stacksTo(1)) {
+    override fun getBarColor(stack: ItemStack): Int = 0x9f009f
+    override fun isBarVisible(stack: ItemStack): Boolean = true
+    override fun getBarWidth(stack: ItemStack): Int = 8
 }
