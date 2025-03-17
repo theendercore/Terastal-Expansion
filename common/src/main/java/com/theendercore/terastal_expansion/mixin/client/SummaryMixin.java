6f5e9740d38414657b017d61a986504e86f501f3
TheEnderCore <theendercore@gmail.com>
2025-03-17 18:05:06 +0200

    - Summery Screen Tera Icon rendering

--- common/src/main/java/com/theendercore/terastal_expansion/mixin/client/SummaryMixin.java
+++ common/src/main/java/com/theendercore/terastal_expansion/mixin/client/SummaryMixin.java
@@ -2,9 +2,12 @@ package com.theendercore.terastal_expansion.mixin.client;
 
 import com.cobblemon.mod.common.client.gui.summary.Summary;
 import com.cobblemon.mod.common.pokemon.Pokemon;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static com.cobblemon.mod.common.client.render.RenderHelperKt.drawScaledText;
+import static com.theendercore.terastal_expansion.client.RenderingKt.summeryScreenIcon;
 import static com.theendercore.terastal_expansion.misc.MixinKt.getTerastallizedType;
 
 @Mixin(value = Summary.class, remap = false)
@@ -20,22 +24,24 @@ public abstract class SummaryMixin extends Screen {
     @Shadow
     public Pokemon selectedPokemon;
 
+    @Shadow @Final private static float SCALE;
+
     protected SummaryMixin(Component title) {
         super(title);
     }
 
 
     @Inject(method = "render", at = @At("TAIL"))
-    void x(GuiGraphics ctx, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        var x = (width - Summary.BASE_WIDTH) / 2;
-        var y = (height - Summary.BASE_HEIGHT) / 2;
+    void x(GuiGraphics ctx, int mouseX, int mouseY, float delta,
+           CallbackInfo ci, @Local(ordinal = 2) int x, @Local(ordinal = 3) int y, @Local PoseStack matrices) {
         var text = Component.literal("Tera : ");
         var text2 = Component.literal("Terastallized Type : ");
         var state = getTerastallizedType(this.selectedPokemon);
         if (state != null) text2.append(state.getDisplayName());
         else text2.append("None");
+        var type = this.selectedPokemon.getTeraType();
         drawScaledText(
-                ctx, text.append(this.selectedPokemon.getTeraType().getDisplayName()).getVisualOrderText(),
+                ctx, text.append(type.getDisplayName()).getVisualOrderText(),
                 x + 39, y + 148,
                 1f, 1f, 1f, 0xffffff,
                 true, true
@@ -46,5 +52,7 @@ public abstract class SummaryMixin extends Screen {
                 1f, 1f, 1f, 0xffffff,
                 true, true
         );
+
+        summeryScreenIcon(ctx, type, matrices, x, y);
     }
 }
--- common/src/main/kotlin/com/theendercore/terastal_expansion/client/Rendering.kt
+++ common/src/main/kotlin/com/theendercore/terastal_expansion/client/Rendering.kt
@@ -1,9 +1,15 @@
 package com.theendercore.terastal_expansion.client
 
+import com.cobblemon.mod.common.api.gui.blitk
 import com.cobblemon.mod.common.api.types.tera.TeraType
+import com.cobblemon.mod.common.api.types.tera.elemental.ElementalTypeTeraType
+import com.cobblemon.mod.common.client.gui.TypeIcon
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.mojang.blaze3d.vertex.PoseStack
+import com.theendercore.terastal_expansion.TerastalConst.id
 import com.theendercore.terastal_expansion.misc.getParticle
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.GuiGraphics
 
 fun teraTypeRenderer(entity: PokemonEntity, terastallizedType: TeraType) {
     if (Minecraft.getInstance().isPaused) return
@@ -23,5 +29,25 @@ fun teraTypeRenderer(entity: PokemonEntity, terastallizedType: TeraType) {
         entity.z + (random.nextDouble() - 0.5) * offset,
         0.0, 0.0, 0.0
     )
+}
 
+private val teraResource = id("textures/gui/summary/tera.png")
+fun summeryScreenIcon(ctx: GuiGraphics, type: TeraType, matrices: PoseStack, x: Int, y: Int) {
+    val scale2 = 1f //0.9375f
+    blitk(
+        matrixStack = matrices,
+        texture = teraResource,
+        x = (x - 24) / scale2,
+        y = (y + 120) / scale2,
+        width = 25,
+        height = 25,
+        scale = scale2
+    )
+    if (type is ElementalTypeTeraType) TypeIcon(
+        x = x - 10.5,
+        y = y + 123.5,
+        type = type.type,
+        secondaryType = null,
+        centeredX = true
+    ).render(ctx)
 }
